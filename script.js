387b7ad77fa5570f9bccd46f9135dee0e6f3004b
swamptoday <maryna.nyzova.sa.2021@lpnu.ua>
2022-09-30 17:06:29 +0300

    adding css and comments to html and js

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+//element, where words will be placed
 var word_place = document.getElementById("myPara");
 
+//when button clicked
 function changeText()
 {
+    //variable to keep text
     var name = getText();
 
+    //variable to keep speed
     var speed = getWPM();
 
+    //creating array of words by splitting text
     const ready_to_read = name.split(" ");
 
+    //sending array and speed to display function
     displayWords(ready_to_read, speed);
 
 }
 
+//getting text
 function getText()
 {
     var text = window.prompt("Enter your text: ");
     return text;
 }
 
+
+//getting speed
 function getWPM()
 {
     var speed = window.prompt("Enter WPM: ");
@@ -30,10 +39,13 @@ function getWPM()
     return speed;
 }
 
+//function for displaying words with needed speed
 async function displayWords(text, speed)
 {
-    var s_speed = (60/speed);
-    var ms_speed = s_speed * 1000;
+    //getting speed in milliseconds for sleep function
+    var ms_speed = (60/speed) * 1000;
+
+    //displaying words from array one by one
     for(var i = 0; i < text.length; i++)
     {
         var word = text[i];
@@ -45,6 +57,7 @@ async function displayWords(text, speed)
 
 }
 
+//creating sleep function with milliseconds as parameter
 function sleep(ms) 
 {
     return new Promise(resolve => setTimeout(resolve, ms));
